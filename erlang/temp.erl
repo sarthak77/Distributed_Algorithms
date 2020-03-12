6b552e5382c658a4c704c3df83432429c48dcab6
sarthak singhal <sarthak.singhal@students.iiit.ac.in>
2020-03-12 11:14:15 +0530

    updates

--- erlang/temp.erl
+++ erlang/temp.erl
@@ -1,5 +1,6 @@
 -module(temp).
--export([main/1,merge_sort/1,for/2]).
+-import(lists,[nth/2]). 
+-export([main/1,merge_sort/1,for/2,f/1]).
 
 merge_sort([L]) -> [L]; 
 
@@ -9,13 +10,19 @@ merge_sort(L)   ->
 
 
 
+f(N) ->
+    io:fwrite("~p~n",[N]).
+
+
+
 for(0,_) -> 
     
     []; 
    
 for(N,Term) when N > 0 -> 
    
-    io:fwrite("~p~n",[N]), 
+    % io:fwrite("~p~n",[nth(N,Term)]),
+    PID=spawn(temp,f,[N]),
    [Term|for(N-1,Term)]. 
 
 
@@ -23,25 +30,20 @@ for(N,Term) when N > 0 ->
 main(Args) ->
 
     [IP,OP]=Args,
-    NOP=2,
 
     {ok, Binary} = file:read_file(IP),
     Lines = string:tokens(erlang:binary_to_list(Binary), " "),
 
-    A=[lists:sublist(Lines, X, NOP) || X <- lists:seq(1,length(Lines),NOP)],
+    NOP=4,
+    LS=ceil(length(Lines)/NOP),
 
-    io:fwrite("~p~n",[A]),
+    A=[lists:sublist(Lines, X, LS) || X <- lists:seq(1,length(Lines),LS)],
 
-    for(NOP,A).
+    % io:fwrite("~p~n",[length(A)]),
+    % io:fwrite("~p~n",[A]),
+
+    for(length(A),A).
 
     % M=merge_sort(Lines),
     % io:fwrite("~p~n",[Lines]),
     % io:fwrite("~p~n",[M]).
-
-
-
-
-
-
-
-
